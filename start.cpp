b456ef14125365277d8f65358aadb87d41486cc5
Akash <joinakd@gmail.com>
2021-08-14 00:55:09 +0530

    first commit

---
+++ start.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define S second
+#define F first
+#define fo(i,n) for(ll i=0;i<n;i++)
+#define foe(i,n) for(ll i=1;i<=n;i++)
+#define PI 3.141592654
+#define vi vector<int>
+#define vl vector<ll>
+#define all(v) (v).begin(), (v).end()
+#define allr(v) (v).rbegin(), (v).rend()
+#define PB push_back
+#define MP make_pair
+#define pi pair<int,int>
+#define pl pair<ll,ll>
+#define mod 1e9+7
+#define mod 1e9+7
+#define N 100010
+typedef long long ll;
+typedef long double ld;
+
+void solve(){
+
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    //int _t; cin>>_t; while(_t--)
+    solve();
+}
